3762699d9f4eb5ee3877e1adaeb0ff2eaa205917
matias <matidae@gmail.com>
2016-04-21 02:13:36 -0300

    starting plot

--- pca_freq_nt.R
+++ pca_freq_nt.R
@@ -11,10 +11,17 @@ calc_freqs <- function(file_name, kmer) {
   #Count kmer and mirror sequences to avoid strand bias
   count_plus <- oligonucleotideFrequency(fasta, kmer)
   count_minus <- oligonucleotideFrequency(reverseComplement(fasta), kmer)
-  #Calculate kmers' frequencies as dataframe
+  #Calculate kmers' frequencies as dataframe.
   kmer_freqs <- as.data.frame(cbind(prop.table(count_plus + count_minus, 1)))
+  #Get seq names
+  seq_names <- names(fasta)
   #Add sequence names
   row.names(kmer_freqs) <- seq_names
   return(kmer_freqs)
 }
+
+kmer_freqs <- calc_freqs(file_name, kmer)
+pca <- prcomp(kmer_freqs, scale = TRUE, center = TRUE)
+pca_data <- data.frame(PC1 = pca$x[,1], PC2 = pca$x[,2])
+ggplot(pca_data, mapping = aes(PC1, PC2)) + geom_point()
 
