499115dc0e836333c50317bd9426a390c4352ead
matias <matidae@gmail.com>
2016-04-20 23:56:13 -0300

    --

--- pca_freq_nt.R
+++ pca_freq_nt.R
@@ -1,25 +1,14 @@
 require(Biostrings)
 
+#Define kmer size and file name
 kmer = 3
-base = '/home/matias/assemblies/'
-fa_list = c('file2.fa','file1.fa')
+file_name = '/home/matias/projects/r_scripts/m.all.fasta'
 
-calc_freqs <- function(base, fa_list, kmer) {
-  freq_all=NULL
-  for (i in fa_list){
-    orfs <- readDNAStringSet(paste(base,"/",i,sep=""), "fasta")
-    count_plus <- oligonucleotideFrequency(orfs, kmer)
-    count_minus <- oligonucleotideFrequency(reverseComplement(orfs), kmer)
-    seq_names <- names(orfs)   
-    if (!is.null(freq_all)) {      
-      freq_tmp <- as.data.frame(cbind(prop.table(count_plus + count_minus, 1)))
-      row.names(freq_tmp)<-seq_names
-      freq_all <- rbind(freq_all,freq_tmp)
-    } 
-    else {
-      freq_all <- as.data.frame(cbind(prop.table(count_plus + count_minus, 1)), grp=i)
-      row.names(freq_all)<-seq_names
-    }    
-  }
-  return(freq_all)
-}
+#Function that determines the frequency of kmer in a given string
+calc_freqs <- function(file_name, kmer) {
+  #Load fasta file
+  fasta <- readDNAStringSet(file_name, "fasta")
+  #Mirror sequences to avoid strand bias
+  count_plus <- oligonucleotideFrequency(orfs, kmer)
+  count_minus <- oligonucleotideFrequency(reverseComplement(orfs), kmer)
+
