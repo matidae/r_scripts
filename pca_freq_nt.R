154bfeda035b93496c86a1f9115e4f669974b1fa
matias <matidae@gmail.com>
2016-04-24 23:42:43 -0300

    add z value

--- pca_freq_nt.R
+++ pca_freq_nt.R
@@ -1,10 +1,11 @@
 require(Biostrings)
+require (ggplot2)
 
 #Define kmer size and file name
 kmer = 3
-file_name = '/home/matias/projects/r_scripts/m.all.fasta'
+file_name = '/home/matias/projects/r_scripts/mt1.all.fasta'
 
-#Function that determines the frequency of kmer in a collection of strings (aka multifasta)
+#Function that determines the frequency of kmer in a collection of strings (i.e. multifasta)
 calc_freqs <- function(file_name, kmer) {
   #Load fasta file
   fasta <- readDNAStringSet(file_name, "fasta")
@@ -23,8 +24,12 @@ calc_freqs <- function(file_name, kmer) {
 kmer_freqs <- calc_freqs(file_name, kmer)
 #Calculate PCA 
 pca <- prcomp(kmer_freqs, scale = TRUE, center = TRUE)
+#Load z values (i.e. sequence's properties)
+z_value<-read.table(file='/home/matias/projects/r_scripts/gcindex', header=FALSE)$V2
 #Create dataframe with the scores of the two first components
-pca_data <- data.frame(PC1 = pca$x[,1], PC2 = pca$x[,2])
+pca_data <- data.frame(PC1 = pca$x[,1], PC2 = pca$x[,2], z_val =z_value)
 #Plot the data
-ggplot(pca_data, mapping = aes(PC1, PC2)) + geom_point()
+ggplot(data = pca_data, mapping = aes(PC1, PC2)) + 
+  geom_point(shape=19, size=1.5, aes(colour=z_val))
+
 
