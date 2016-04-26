529cd7217a3d62abea94f6506c26abf2d523f5d6
matias <matidae@gmail.com>
2016-04-25 23:49:54 -0300

    min fix scale

--- pca_freq_nt.R
+++ pca_freq_nt.R
@@ -27,11 +27,11 @@ pca <- prcomp(kmer_freqs, scale = TRUE, center = TRUE)
 #Load z values (i.e. sequence's properties)
 z_value<-read.table(file='/home/matias/projects/r_scripts/gcindex', header=FALSE)$V2
 #Create dataframe with the scores of the two first components
-pca_data <- data.frame(PC1 = pca$x[,1], PC2 = pca$x[,2], z_val =z_value)
+pca_data <- data.frame(PC1 = pca$x[,1], PC2 = pca$x[,2], z_val = z_value)
 #Plot the data
 ggplot(data = pca_data, mapping = aes(PC1, PC2)) + 
   geom_point(shape=19, size=1.5, aes(colour=z_val)) +
-  scale_colour_gradientn(colours=c("darkgreen","green","orange", "darkred"), limits=c(0,1)) +
+  scale_colour_gradientn(colours=c("darkgreen","green","orange", "darkred"), limits=c(min(z_value),max(z_value))) +
   labs(colour=" z val", x="PC1", y="PC2", title="PCA spp") +
   theme(plot.title=element_text(face="bold"))
 
